203176e79eef16f0f320b1142456240af8121fe0
samoyla <samoyla@hotmail.com>
2023-10-30 15:30:17 +0100

    RESTful Web Service

---
+++ service/src/main/java/com/restful_web_service/service/Greeting.java
@@ -0,0 +1,5 @@
+package com.restful_web_service.service;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ service/src/test/java/com/restful_web_service/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful_web_service.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
